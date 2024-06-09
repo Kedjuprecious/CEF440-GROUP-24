034d1cdb0c0ab794dd788567dba014873a27922f
Wendydjoumessi <133278891+wendydjoumessi@users.noreply.github.com>
2024-06-09 12:26:26 +0100

    Added FindAm

---
+++ findam/app/(tabs)/Report.jsx
@@ -0,0 +1,10 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+export default function Report() {
+  return (
+    <View>
+      <Text>Report if there is any problem</Text>
+    </View>
+  )
+}
--- findam/app/_layout.tsx
+++ findam/app/_layout.tsx
@@ -1,37 +1,17 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
-import { useFonts } from 'expo-font';
-import { Stack } from 'expo-router';
-import * as SplashScreen from 'expo-splash-screen';
-import { useEffect } from 'react';
-import 'react-native-reanimated';
+import { View, Text } from 'react-native'
+import React from 'react'
+import { Stack } from "expo-router";
+import { useFonts } from "expo-font";
 
-import { useColorScheme } from '@/hooks/useColorScheme';
-
-// Prevent the splash screen from auto-hiding before asset loading is complete.
-SplashScreen.preventAutoHideAsync();
-
-export default function RootLayout() {
-  const colorScheme = useColorScheme();
-  const [loaded] = useFonts({
-    SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
-  });
-
-  useEffect(() => {
-    if (loaded) {
-      SplashScreen.hideAsync();
-    }
-  }, [loaded]);
-
-  if (!loaded) {
-    return null;
-  }
-
+export default function _layout() {
+  useFonts({
+    'outfit':require('../assets/fonts/Outfit-Regular.ttf'),
+    'outfit-medium':require('../assets/fonts/Outfit-Medium.ttf'),
+    'outfit-bold':require('../assets/fonts/Outfit-Bold.ttf'),
+    })
   return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="+not-found" />
-      </Stack>
-    </ThemeProvider>
-  );
+    <Stack>
+      <Stack.Screen name="(tabs)"/>
+    </Stack>
+  )
 }
